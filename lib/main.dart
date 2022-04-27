97d87287044efda7bd6119e2b2952ef8a89f957b
FlutLab.io <>
2022-04-27 19:24:48 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final appTitle = 'Farmacia';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: appTitle,
+      home: MyHomePage(title: appTitle),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        centerTitle: true,
+        backgroundColor: Colors.lightBlue[300],
+        automaticallyImplyLeading: true,
+      ),
+      body: ListView(
+        children: [
+          Image.asset('assets/E1.jpg'),
+          Image.asset('assets/E2.jpg'),
+          Image.asset('assets/E3.jpg'),
+        ], //children
+      ),
+      drawer: Drawer(
+        // Agrega un ListView al drawer. Esto asegura que el usuario pueda desplazarse
+        // a través de las opciones en el Drawer si no hay suficiente espacio vertical
+        // para adaptarse a todo.
+        child: ListView(
+          // Importante: elimina cualquier padding del ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Encabezado de Drawer'),
+              decoration: BoxDecoration(
+                color: Colors.lightBlue[200],
+              ),
+            ),
+            ListTile(
+              title: Text('Medicamento 1'),
+              onTap: () {
+                // Actualiza el estado de la aplicación
+                // ...
+                // Luego cierra el drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Medicamento 2'),
+              onTap: () {
+                // // Actualiza el estado de la aplicación
+                // ...
+                // Luego cierra el drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Medicamento 3'),
+              onTap: () {
+                // // Actualiza el estado de la aplicación
+                // ...
+                // Luego cierra el drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
